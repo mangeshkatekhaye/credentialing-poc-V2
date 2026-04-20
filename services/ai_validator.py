a52aa2ebe2af641ab8f10c65755703165d5f5df6
mangeshkatekhaye <mangesh.katekhaye@gmail.com>
2026-04-20 22:46:51 +0530

    Update ai_validator.py

--- services/ai_validator.py
+++ services/ai_validator.py
@@ -1,15 +1,14 @@
 # services/ai_validator.py
 
 from openai import OpenAI
-import os
-from dotenv import load_dotenv
-from pathlib import Path
+import streamlit as st
 
-# -------------------------------
-# LOAD ENV FILE
-# -------------------------------
-env_path = Path(__file__).resolve().parent.parent / ".env"
-load_dotenv(dotenv_path=env_path)
+api_key = st.secrets.get("OPENAI_API_KEY")
+
+if not api_key:
+    client = None
+else:
+    client = OpenAI(api_key=api_key)
 
 # -------------------------------
 # INITIALIZE CLIENT
